a2572c6eacfc10c1dc7553ff3d05f30e8eeb0432
IRUoi <pretvorniki@gmail.com>
2022-10-31 15:55:45 +0800

    cms 主程序入口

---
+++ cms/src/main/java/com/briup/cms/CmsApplication.java
@@ -0,0 +1,16 @@
+package com.briup.cms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Auther: ZHU(lc))
+ * @Date: 10/31/2022-10-31-1:24 PM
+ * @Description：com.briup.cms
+ */
+@SpringBootApplication
+public class CmsApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CmsApplication.class,args);
+    }
+}
